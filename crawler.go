f7cc50041c8b481d89064c01110b8d5e4dce3636
Sergey Serebryakov <sergey@serebryakov.info>
2017-05-08 01:42:03 -0700

    Refactoring + comments

--- crawler.go
+++ crawler.go
@@ -1,253 +1,318 @@
 package main
 
 import (
-  "errors"
-  "fmt"
-  "io"
-  "log"
-  "net/http"
-  urllib "net/url"
-  "os"
-  "strings"
-  "sync"
-  "time"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	urllib "net/url"
+	"os"
+	"strings"
+	"sync"
+	"time"
 
-  "golang.org/x/net/html"
+	"golang.org/x/net/html"
 )
 
-type foundLink struct {
-  url    *urllib.URL
-  parent string
+type crawlTask struct {
+	url    *urllib.URL
+	parent string
 }
 
-func findAttr(token html.Token, key string) (string, error) {
-  for _, attr := range token.Attr {
-    if attr.Key == key {
-      return attr.Val, nil
-    }
-  }
-  return "", errors.New("No " + key + " attribute found")
+// Given HTML tag and attribute name, returns value of that attribute or error
+// if no such attribute was found.
+func findAttr(tag html.Token, name string) (string, error) {
+	for _, attr := range tag.Attr {
+		if attr.Key == name {
+			return attr.Val, nil
+		}
+	}
+	return "", errors.New("No " + name + " attribute found")
 }
 
-func parseHTML(htmlReader io.Reader, base *urllib.URL, taskChan chan<- foundLink, legalHashLinksChan chan<- string, tasks *sync.WaitGroup) {
-  tokenizer := html.NewTokenizer(htmlReader)
+// Given a HTML body, parses it and returns a set of hrefs (as in <a href="...">)
+// and a set of allowed #fragments that other pages can use when linking to this page.
+func parseHTML(htmlReader io.Reader) (map[*urllib.URL]bool, map[string]bool) {
 
-  for {
-    event := tokenizer.Next()
+	foundHrefs := make(map[*urllib.URL]bool)
+	allowedHashtags := make(map[string]bool)
 
-    switch {
-    case event == html.ErrorToken:
-      // End of the document, we're done
-      return
-    case event == html.StartTagToken:
-      token := tokenizer.Token()
+	tokenizer := html.NewTokenizer(htmlReader)
 
-      isAnchor := token.Data == "a"
-      if isAnchor {
+	for {
+		event := tokenizer.Next()
 
-        id, errNoID := findAttr(token, "id")
-        if errNoID == nil {
-          legalHashLinksChan <- base.String() + "#" + id
-        }
+		switch {
+		case event == html.ErrorToken:
+			// End of the document, we're done
+			return foundHrefs, allowedHashtags
 
-        name, errNoName := findAttr(token, "name")
-        if errNoName == nil {
-          legalHashLinksChan <- base.String() + "#" + name
-        }
+		case event == html.StartTagToken:
+			token := tokenizer.Token()
 
-        href, errNoHref := findAttr(token, "href")
-        if errNoHref == nil {
-          hrefURL, err := urllib.Parse(href)
-          if err != nil {
-            log.Printf("Unparseable href: %s\n", token.String())
-          } else {
-            resolvedURL := base.ResolveReference(hrefURL)
+			id, errNoID := findAttr(token, "id")
+			if errNoID == nil {
+				allowedHashtags["#"+id] = true
+			}
 
-            tasks.Add(1)
-            taskChan <- foundLink{resolvedURL, base.String()}
-          }
-        }
+			isAnchor := token.Data == "a"
+			if isAnchor {
+				name, errNoName := findAttr(token, "name")
+				if errNoName == nil {
+					allowedHashtags["#"+name] = true
+				}
 
-        if errNoID != nil && errNoName != nil && errNoHref != nil {
-          //log.Printf("Useless link %s\n", token.String())
-        }
-      } else {
-        id, errNoID := findAttr(token, "id")
-        if errNoID == nil {
-          legalHashLinksChan <- base.String() + "#" + id
-        }
-      }
-    }
-  }
+				href, errNoHref := findAttr(token, "href")
+				if errNoHref == nil {
+					hrefURL, err := urllib.Parse(href)
+					if err == nil {
+						foundHrefs[hrefURL] = true
+					} else {
+						log.Printf("Unparseable href: %s\n", token.String())
+					}
+				}
+
+				if errNoID != nil && errNoName != nil && errNoHref != nil {
+					//log.Printf("Useless link %s\n", token.String())
+				}
+			}
+		}
+	}
 }
 
-func crawl(task foundLink, goDeeper bool, taskChan chan<- foundLink, legalHashLinksChan chan<- string, tasks *sync.WaitGroup) {
-  defer tasks.Done()
+// Given a link, tries to access it. If unsuccessful, submits an async error message.
+// If successful and should recurse, parses the page and submits new crawl tasks.
+func crawl(
+	link crawlTask,
+	tasksLatch *sync.WaitGroup,
+	shouldRecurse bool,
+	brokenLinkMessagesChan chan<- string,
+	allowedHashLinksChan chan<- string,
+	crawlTasksChan chan<- crawlTask) {
 
-  url := task.url
-  //log.Printf("Crawling %s\n", url.String())
-  resp, err := http.Get(url.String())
-  if err != nil {
-    log.Printf("Failed to GET %s on %s: %s\n", url.String(), task.parent, err.Error())
-    return
-  }
+	defer tasksLatch.Done()
 
-  if resp.StatusCode != 200 {
-    log.Printf("Dead link (%d) %s on %s\n", resp.StatusCode, url.String(), task.parent)
-    return
-  }
+	url := link.url
 
-  if goDeeper {
-    htmlReader := resp.Body
-    defer htmlReader.Close()
+	//log.Printf("Crawling %s\n", url.String())
+	resp, err := http.Get(url.String())
+	if err != nil {
+		brokenLinkMessagesChan <- fmt.Sprintf("Failed to GET: %s on %s: %s", url.String(), link.parent, err.Error())
+		return
+	}
 
-    parseHTML(htmlReader, resp.Request.URL, taskChan, legalHashLinksChan, tasks)
-  }
+	if resp.StatusCode != 200 {
+		brokenLinkMessagesChan <- fmt.Sprintf("Dead link: %s on %s: HTTP code %d", url.String(), link.parent, resp.StatusCode)
+		return
+	}
+
+	if shouldRecurse {
+		htmlReader := resp.Body
+		defer htmlReader.Close()
+
+		foundHrefs, allowedHashtags := parseHTML(htmlReader)
+
+		baseURL := resp.Request.URL
+		baseURLString := baseURL.String()
+
+		for allowedHashtag := range allowedHashtags {
+			allowedHashLinksChan <- baseURLString + allowedHashtag
+		}
+
+		for hrefURL := range foundHrefs {
+			resolvedURL := baseURL.ResolveReference(hrefURL)
+			tasksLatch.Add(1)
+			crawlTasksChan <- crawlTask{resolvedURL, baseURLString}
+		}
+	}
 }
 
+// Whether a URL is a sub-URL of any of the seed URLs.
 func isDescendant(url string, seedURLs []string) bool {
-  for _, seedURL := range seedURLs {
-    if strings.HasPrefix(url, seedURL) {
-      return true
-    }
-  }
-  return false
+	for _, seedURL := range seedURLs {
+		if strings.HasPrefix(url, seedURL) {
+			return true
+		}
+	}
+	return false
 }
 
-func extractPage(url *urllib.URL) *urllib.URL {
-  parsedURL, err := urllib.Parse(url.Scheme + "://" + url.Host + url.Path + url.RawQuery)
-  if err != nil {
-    log.Printf("Failed to parse: %s", err.Error())
-  }
-  return parsedURL
+// Given a URL, removes the #fragment part.
+func stripHashtagFromURL(url *urllib.URL) *urllib.URL {
+	parsedURL, err := urllib.Parse(url.Scheme + "://" + url.Host + url.Path + url.RawQuery)
+	if err != nil {
+		log.Printf("Failed to parse: %s", err.Error())
+	}
+	return parsedURL
 }
 
-func eventLoop(taskChan <-chan foundLink, process func(foundLink), finishedChan chan<- bool) {
-  for task := range taskChan {
-    //log.Println("processing")
-    process(task)
-  }
+// Given a channel of tasks and a handler, invokes the handler on every task.
+func eventLoop(
+	crawlTasksChan <-chan crawlTask,
+	handler func(crawlTask),
+	doneChan chan<- bool) {
 
-  log.Print("Done listening\n")
-  finishedChan <- true
+	for crawlTask := range crawlTasksChan {
+		//log.Println("processing")
+		handler(crawlTask)
+	}
+
+	doneChan <- true
 }
 
-func acceptLegalHashLinks(legalHashLinksChan <-chan string, finishedChan chan<- map[string]bool) {
-  legalHashLinks := make(map[string]bool)
+// Given a channel of strings, builds a set of them and submits to the output channel.
+// Optionally prints the strings upon receiving.
+func accumulateChan(
+	messagesChan <-chan string,
+	shouldPrint bool,
+	doneChan chan<- map[string]bool) {
 
-  for legalHashLink := range legalHashLinksChan {
-    legalHashLinks[legalHashLink] = true
-  }
+	messages := make(map[string]bool)
 
-  finishedChan <- legalHashLinks
+	for message := range messagesChan {
+		if shouldPrint {
+			log.Println(message)
+		}
+		messages[message] = true
+	}
+
+	doneChan <- messages
+}
+
+// Whether the URL looks like URL and doesn't contain known-to-fail parts.
+func isValidURL(url string) bool {
+	return (strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) &&
+		!strings.Contains(url, "_history") && !strings.Contains(url, "rbcommons")
+}
+
+// Whether the URL is a sub-URL of any of the seed URLs.
+func shouldCrawlURL(url string, seedURLs []string) bool {
+	return isDescendant(url, seedURLs)
 }
 
 func main() {
 
-  if len(os.Args) < 2 {
-    fmt.Fprintf(os.Stderr, "Usage: %s URL [URL...]\n", os.Args[0])
-    os.Exit(1)
-  }
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s URL [URL...]\n", os.Args[0])
+		os.Exit(1)
+	}
 
-  log.Print("Hello\n")
+	log.Println("Hello")
 
-  seedURLs := os.Args[1:]
+	seedURLs := os.Args[1:]
 
-  // accept hash links
-  legalHashLinkChan := make(chan string)
-  finishedHashLinksChan := make(chan map[string]bool)
-  go acceptLegalHashLinks(legalHashLinkChan, finishedHashLinksChan)
+	// remember broken link messages
+	brokenLinkMessagesChan := make(chan string)
+	doneBrokenLinkMessagesChan := make(chan map[string]bool)
+	go accumulateChan(brokenLinkMessagesChan, true, doneBrokenLinkMessagesChan)
 
-  taskChan := make(chan foundLink)
-  var tasks sync.WaitGroup
+	// remember allowed hash links
+	allowedHashLinksChan := make(chan string)
+	doneHashLinksChan := make(chan map[string]bool)
+	go accumulateChan(allowedHashLinksChan, false, doneHashLinksChan)
 
-  isValidURL := func(url string) bool {
-    return (strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) &&
-      !strings.Contains(url, "_history") && !strings.Contains(url, "rbcommons")
-  }
+	crawlTasksChan := make(chan crawlTask)
+	var tasksLatch sync.WaitGroup
 
-  isCrawlableURL := func(url string) bool {
-    return isDescendant(url, seedURLs)
-  }
+	visitedLinks := make(map[string]bool)
+	foundLinksWithHashes := make(map[crawlTask]bool)
+	ignoredURLs := make(map[string]bool)
 
-  visitedPages := make(map[string]bool)
-  hashLinks := make(map[foundLink]bool)
-  ignoredURLs := make(map[string]bool)
+	processFoundLink := func(task crawlTask) {
+		url := task.url
+		urlString := url.String()
 
-  process := func(task foundLink) {
-    url := task.url
-    urlString := url.String()
+		if isValidURL(urlString) {
+			shouldCrawl := shouldCrawlURL(urlString, seedURLs)
+			if shouldCrawl {
+				if url.Fragment != "" && url.Fragment != "start-of-content" {
+					foundLinksWithHashes[task] = true
+				}
+			}
 
-    if isValidURL(urlString) {
-      isCrawlable := isCrawlableURL(urlString)
-      if isCrawlable {
-        if url.Fragment != "" && url.Fragment != "start-of-content" {
-          hashLinks[task] = true
-        }
-      }
+			pageURL := stripHashtagFromURL(url)
+			pageURLString := pageURL.String()
+			if !visitedLinks[pageURLString] {
+				visitedLinks[pageURLString] = true
+				time.Sleep(100 * time.Millisecond)
+				go crawl(task, &tasksLatch, shouldCrawl,
+					brokenLinkMessagesChan, allowedHashLinksChan, crawlTasksChan)
+			} else {
+				tasksLatch.Done()
+			}
+		} else {
+			ignoredURLs[urlString] = true
+			tasksLatch.Done()
+		}
+	}
 
-      pageURL := extractPage(url)
-      pageURLString := pageURL.String()
-      if !visitedPages[pageURLString] {
-        visitedPages[pageURLString] = true
-        time.Sleep(100 * time.Millisecond)
-        go crawl(task, isCrawlable, taskChan, legalHashLinkChan, &tasks)
-      } else {
-        tasks.Done()
-      }
-    } else {
-      ignoredURLs[urlString] = true
-      tasks.Done()
-    }
-  }
+	// start processing
+	eventLoopDoneChan := make(chan bool)
+	go eventLoop(crawlTasksChan, processFoundLink, eventLoopDoneChan)
 
-  // start processing
-  eventLoopFinishedChan := make(chan bool)
-  go eventLoop(taskChan, process, eventLoopFinishedChan)
+	// seed
+	for _, url := range seedURLs {
+		parsedURL, err := urllib.Parse(url)
+		if err != nil {
+			log.Printf("Failed to parse provided URL: %s", err.Error())
+		}
 
-  // seed
-  for _, url := range seedURLs {
-    parsedURL, err := urllib.Parse(url)
-    if err != nil {
-      log.Printf("Failed to parse provided URL: %s", err.Error())
-    }
+		tasksLatch.Add(1)
+		crawlTasksChan <- crawlTask{parsedURL, ""}
+	}
+	log.Println("Submitted all seed URLs")
 
-    tasks.Add(1)
-    taskChan <- foundLink{parsedURL, ""}
-  }
-  log.Println("Submitted")
+	log.Println("Waiting for all tasks to be done")
+	tasksLatch.Wait()
+	log.Println("All tasks are done")
 
-  tasks.Wait()
+	log.Println("Stopping eventloop")
+	close(crawlTasksChan)
+	log.Println("Waiting for eventloop to exit")
+	<-eventLoopDoneChan
+	log.Println("Eventloop exited")
 
-  close(taskChan)
-  <-eventLoopFinishedChan
+	log.Println("Stopping broken links processing")
+	close(brokenLinkMessagesChan)
+	log.Println("Waiting for broken links processing to exit")
+	brokenLinkMessages := <-doneBrokenLinkMessagesChan
+	log.Println("Broken links processing exited")
 
-  close(legalHashLinkChan)
-  legalHashLinks := <-finishedHashLinksChan
-  log.Println("All done")
+	log.Println("Stopping hashlinks processing")
+	close(allowedHashLinksChan)
+	log.Println("Waiting for hashlinks processing to exit")
+	allowedHashLinks := <-doneHashLinksChan
+	log.Println("Hashlink processing exited")
 
-  //log.Println("Crawled:")
-  //for x := range visitedPages {
-  //	log.Println(x)
-  //}
-  log.Println("Broken Hashlinks:")
-  for task := range hashLinks {
-    url := task.url
-    urlString := url.String()
-    userContentModifiedStr := strings.Replace(urlString, "#", "#user-content-", 1)
-    wikiModifiedStr := strings.Replace(urlString, "#wiki-", "#user-content-", 1)
-    if !legalHashLinks[urlString] && !legalHashLinks[userContentModifiedStr] && !legalHashLinks[wikiModifiedStr] {
-      log.Printf("%s on %s", urlString, task.parent)
-    }
-  }
+	log.Println("Crawled links:")
+	for link := range visitedLinks {
+		log.Println(link)
+	}
 
-  //log.Println("Legal Hashlinks:")
-  //for x := range legalHashLinks {
-  //	log.Println(x)
-  //}
+	log.Println("Broken links:")
+	for brokenLinkMessage := range brokenLinkMessages {
+		log.Println(brokenLinkMessage)
+	}
 
-  //log.Println("Ignored:")
-  //for x := range ignoredURLs {
-  //	log.Println(x)
-  //}
+	log.Println("Broken hashlinks:")
+	for task := range foundLinksWithHashes {
+		url := task.url
+		urlString := url.String()
+		userContentModifiedStr := strings.Replace(urlString, "#", "#user-content-", 1)
+		wikiModifiedStr := strings.Replace(urlString, "#wiki-", "#user-content-", 1)
+		if !allowedHashLinks[urlString] && !allowedHashLinks[userContentModifiedStr] && !allowedHashLinks[wikiModifiedStr] {
+			log.Printf("%s on %s", urlString, task.parent)
+		}
+	}
+
+	//log.Println("Allowed Hashlinks:")
+	//for x := range allowedHashLinks {
+	//	log.Println(x)
+	//}
+
+	//log.Println("Ignored:")
+	//for x := range ignoredURLs {
+	//	log.Println(x)
+	//}
 }
